942a2a49b6ac965df5eda871bcddc95855224598
jeabea <123121991+jeabea@users.noreply.github.com>
2024-03-12 19:43:02 +0100

    commit jeanne

--- app/src/main/java/com/example/projetappdesignv2/MainActivity3.kt
+++ app/src/main/java/com/example/projetappdesignv2/MainActivity3.kt
@@ -2,10 +2,33 @@ package com.example.projetappdesignv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+
+
 
 class MainActivity3 : AppCompatActivity() {
+
+    private var counterValue = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        val textViewCounter: TextView = findViewById(R.id.textViewCounter)
+
+        val buttonPlus: ImageButton = findViewById(R.id.my_round_button2)
+        buttonPlus.setOnClickListener {
+            counterValue++
+            textViewCounter.text = counterValue.toString()
+        }
+
+        val buttonMinus: ImageButton = findViewById(R.id.my_round_button)
+        buttonMinus.setOnClickListener {
+            if (counterValue > 1) {
+                counterValue--
+                textViewCounter.text = counterValue.toString()
+            }
+        }
     }
 }
