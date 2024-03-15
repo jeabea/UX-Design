f0d2cc87989de10b88811b68749cef3be04b1f06
jeabea <123121991+jeabea@users.noreply.github.com>
2024-03-15 11:10:39 +0100

    commit jeanne

--- app/src/main/java/com/example/projetappdesignv2/MainActivity6.kt
+++ app/src/main/java/com/example/projetappdesignv2/MainActivity6.kt
@@ -1,11 +1,15 @@
 package com.example.projetappdesignv2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
+import android.view.View
+import android.widget.Button
+
 
 class MainActivity6 : AppCompatActivity() {
 
@@ -19,16 +23,26 @@ class MainActivity6 : AppCompatActivity() {
 
         val elementAAfficher: RelativeLayout = findViewById(R.id.rectangle)
 
-
         if (boutonClique) {
             // Le bouton a été cliqué dans MainActivity3, afficher les éléments
-            elementAAfficher.visibility = ImageView.VISIBLE
-
+            elementAAfficher.visibility = View.VISIBLE
         } else {
             // Le bouton n'a pas été cliqué, masquer ou ajuster les éléments comme nécessaire
-            elementAAfficher.visibility = ImageView.GONE
-
+            elementAAfficher.visibility = View.GONE
         }
+        // Vérifier si l'intent contient l'indicateur que le bouton a été cliqué
+        val boutonClique2 = intent.getBooleanExtra("boutonClique2", false)
+
+        val elementAAfficher2: RelativeLayout = findViewById(R.id.rectangle2)
+
+        if (boutonClique2) {
+            // Le bouton a été cliqué dans MainActivity3, afficher les éléments
+            elementAAfficher2.visibility = View.VISIBLE
+        } else {
+            // Le bouton n'a pas été cliqué, masquer ou ajuster les éléments comme nécessaire
+            elementAAfficher2.visibility = View.GONE
+        }
+
 
 
         counterValue = intent.getIntExtra("compteurValue", 1) // Récupérer la valeur du compteur
@@ -48,6 +62,11 @@ class MainActivity6 : AppCompatActivity() {
                 counterValue--
                 textViewCounter.text = counterValue.toString()
             }
+        }
+        val bouton_vers_page = findViewById<Button>(R.id.buttonGoToBeforePage3)
+        bouton_vers_page.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
         }
     }
 }
